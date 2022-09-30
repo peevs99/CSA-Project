0d7b3c63c6aa3df094d5f3cea1c9d4cbab3ad556
peevs99 <88054944+peevs99@users.noreply.github.com>
2022-09-30 12:35:08 -0400

    initial check

--- CSA/CSA/src/Main.java
+++ CSA/CSA/src/Main.java
@@ -1,5 +1,10 @@
+import Registers.java
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+       
+       String val= get_register_value_string("R0");
+       System.out.println("Hello world!==>"+val);
     }
 }
--- CSA/CSA/src/Registers.java
+++ CSA/CSA/src/Registers.java
@@ -1,3 +1,5 @@
+package Registers;
+
 import java.lang.Math;
 
 public class Registers {
